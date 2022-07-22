d2a127b077527261ef34e9f0dbb629b8d0b133a4
wes-mitchell <wmdrums@gmail.com>
2022-07-22 08:40:36 -0500

    completes listing card initial framework without styling

--- DrumDeals/client/src/components/Listings/Listing.js
+++ DrumDeals/client/src/components/Listings/Listing.js
@@ -1,41 +1,65 @@
 import React from "react";
-import { Card, CardBody } from "reactstrap"
+import { Card, CardBody, ButtonGroup } from "reactstrap"
 import { Link } from "react-router-dom";
+import { Button } from "bootstrap";
 
 export const Listing = ({ }) => {
+
+// ------ For Testing Only ---- Delete When Prop is Passed  
 const listing = {
   id: 1,
   title: "Tama Swingstar",
   condition: "Used",
   price: 30.99,
-  publishDate: "this is a date",
+  publishDate: "10/15/2022",
+  imageUrl: "https://mdbootstrap.com/img/new/standard/city/041.webp",
   userProfile: {
     id: 1,
     name: "Joe"
   }
+  
 }
   return (
     <Card >
-    <p className="text-left px-2">From: {listing.userProfile.firstName}</p>
+    <p className="text-left px-2">From: Name {listing.userProfile.firstName}</p>
     <CardBody>
+    <div className="content-container">
+      <div>
       <p>
         <Link to={`/details/${listing.id}`}>
         <strong>{listing.title}</strong>
         </Link>
       </p>
-      <p>{listing.condition}</p>
-      <p>{listing.price}</p>
-      <p>{listing.publishDate}</p>
-      <Link to={`/listings/details/${listing.id}`}>
-        See Details
-      </Link>
-      <Link to={`/listings/edit/${listing.id}`}>
+      <img
+      src={listing.imageUrl}
+      className='img-thumbnail'
+      alt='...'
+      style={{ maxWidth: '24rem' }}
+    />
+      <p><strong>Condition:</strong> {listing.condition}</p>
+      <p><strong>Price:</strong> {listing.price}</p>
+      <p><strong>Listed On:</strong> {listing.publishDate}</p>
+      </div>
+      <div>
+      <ButtonGroup>
+        <button type="button" class="btn btn-outline-light">
+          <Link to={`/listings/details/${listing.id}`}>
+            See Details
+          </Link>
+        </button>
+        <button type="button" className="btn btn-outline-light">
+      <Link className="edit-button" to={`/listings/edit/${listing.id}`}>
         Edit
       </Link>
-      <br/>
+      </button>
+      <button type="button" className="btn btn-outline-light">
       <Link to={`/listings/delete/${listing.id}`}>
-        Delete Video
+        Delete Listing
       </Link>
+      </button>
+      </ButtonGroup>
+      </div>
+      </div>
     </CardBody>
   </Card>
   )
