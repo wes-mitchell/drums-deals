e5ef8a4cfe86c21d125ce3693da1739e96029a28
wes-mitchell <wmdrums@gmail.com>
2022-07-27 14:14:40 -0500

    completes basic styling

--- DrumDeals/client/src/components/Listings/DeleteListing.js
+++ DrumDeals/client/src/components/Listings/DeleteListing.js
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams, useNavigate, Link } from "react-router-dom";
 import { deleteListing, getListingById } from "../../modules/listingsManager";
-import { Card, CardBody, CardTitle, CardSubtitle, CardText, Button } from "reactstrap"
+import { Card, CardBody, CardTitle, CardSubtitle, CardText, Button, Container, Row, Form, FormGroup, Label} from "reactstrap"
+import { formatDate } from "../../helpers";
 
 
 export const DeleteListing = () => {
@@ -16,7 +17,10 @@ const [listing, setListing] = useState({
     description: '',
     price: '',
     categoryId: 0,
-    imageUrl: ''
+    imageUrl: '',
+    userProfile: {
+      firstName: ''
+    }
   }
 )
 
@@ -36,32 +40,40 @@ useEffect(() => {
   getListing()
 }, [])
 
-  return (
-  <Card
-    style={{
-      width: '18rem'
-    }}
-  >
-  <CardBody>
-    <CardTitle tag="h5">
-      {listing.title}
-    </CardTitle>
-    <CardSubtitle
-      className="mb-2 text-muted"
-      tag="h6"
-    >
-      {listing.description}
-    </CardSubtitle>
-    <CardText>
-      {listing.description}
-    </CardText>
-    <Button type="button" onClick={handleDelete}>
-      Delete
-    </Button>
-    <Button type="button" onClick={() => navigate(`/listings/mylistings`)}>
-      Cancel
-    </Button>
-  </CardBody>
-</Card>
+return (
+  <>
+    <Row className="text-center">
+      <h4>Are you sure you'd like to delete this listing?</h4>
+    </Row>
+    <div style={{display: "flex", justifyContent: "center"}}>
+      <Card style={{ width: '18rem'}}>
+        <CardBody>
+        <CardTitle tag="h5" className="text-center">
+            <Link to={`/listings/details/${listing.id}`}>
+              <b>{listing.title}</b>
+            </Link>
+          </CardTitle>
+          <hr/>
+        <CardSubtitle className="mb-2 text-muted" tag="h6">
+          <b style={{color: "black"}}>Description:</b><br/>
+          {listing.description}
+        </CardSubtitle>
+        <CardText>
+          <b>Condition:</b> {listing.condition} <br/>
+          <b>Price:</b> ${listing.price} <br/>
+          <b>Listed On:</b> {formatDate(listing.publishDate)} <br/>
+        </CardText>
+        <div style={{display: "flex", flexDirection: "row", justifyContent: "space-between"}}>
+          <Button color="danger" type="button" onClick={handleDelete}>
+            Delete
+          </Button>
+          <Button color="secondary" type="button" onClick={() => navigate(`/listings/mylistings`)}>
+            Cancel
+          </Button>
+        </div>
+        </CardBody>
+      </Card>
+    </div>
+  </>
   )
 }
