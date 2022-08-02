67c48fd7db837e73b2f241e2c010fa57c6844e46
wes-mitchell <wmdrums@gmail.com>
2022-08-02 15:28:07 -0500

    sets up framework for offers with model class and repo

---
+++ DrumDeals/Controllers/OfferController.cs
@@ -0,0 +1,22 @@
+﻿using DrumDeals.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DrumDeals.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OfferController : ControllerBase
+    {
+        private readonly IOfferRepository _offerRepository;
+        private readonly IListingRepository _listingRepository;
+
+        public OfferController(IOfferRepository offerReop, IListingRepository listingRepo)
+        {
+            _offerRepository = offerReop;
+            _listingRepository = listingRepo;
+        }
+    }
+}
---
+++ DrumDeals/Repositories/OfferRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Configuration;
+using DrumDeals.Models;
+using System.Collections.Generic;
+using DrumDeals.Utils;
+
+namespace DrumDeals.Repositories
+{
+    public class OfferRepository : BaseRepository, IOfferRepository
+    {
+        public OfferRepository(IConfiguration configuration) : base(configuration) { }
+
+        public void Add(Offer offer)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Offer> GetAllOffers()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Offer GetOfferById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Offer> GetOffersByListingId(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Offer> GetOffersByUserId(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update(Offer offer)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
